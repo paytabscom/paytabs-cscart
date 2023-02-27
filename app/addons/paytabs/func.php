855c43f4337cf606d3c2f07238f1a7451204fc5a
orcl.masr@hotmail.com <orcl.masr@hotmail.com>
2023-02-27 12:48:25 +0200

    cleaning the code

--- app/addons/paytabs/func.php
+++ app/addons/paytabs/func.php
@@ -24,37 +24,3 @@ function fn_paytabs_uninstall()
 {
     db_query("DELETE FROM ?:payment_processors WHERE processor_script = ?s", "paytabs.php");
 }
-
-function fn_paytabs_change_order_status(&$status_to, $status_from, $order_info, $force_notification, $order_statuses, $place_order)
-{
-//    $paytabs = new PayTabs();
-//
-//    $processor_data = fn_get_processor_data($order_info['payment_id']);
-//
-//    $currency_f = CART_SECONDARY_CURRENCY;
-//    if ($processor_data['processor_params']['currency'] == 'shop_cur') {
-//        $amount = fn_format_price_by_currency($order_info['total']);
-//    } else {
-//        $amount = fn_format_price($order_info['total'], $processor_data['processor_params']['currency']);
-//        $currency_f = $processor_data['processor_params']['currency'];
-//    }
-//
-//    if ($processor_data['processor_params']['status_hold'] == $status_from && $status_to == $processor_data['processor_params']['paid_order_status']) {
-//        $payment_data = [
-//            'order_id' => ($order_info['payment_info']['order_id']) ? $order_info['payment_info']['order_id'] : $order_info['payment_info']['paytabs_order_id'],
-//            'currency' => $currency_f,
-//            'amount' => round($amount * 100),
-//            'merchant_id' => $processor_data['processor_params']['merchant_id'],
-//        ];
-//
-//        $payment_data['signature'] = $paytabs->getSignature($payment_data, $processor_data['processor_params']['password']);
-//        $response = $paytabs->generatePayTabsUrl($payment_data, true);
-//
-//        if ($response['response_status'] == 'success') {
-//            fn_finish_payment($_REQUEST['order_id'], ['response_status' => $response['response_status']]);
-//        } else {
-//            $status_to = 'F';
-//            fn_update_order_payment_info($_REQUEST['order_id'], ['order_id' => $payment_data['order_id'], 'request_id' => $response['request_id'], 'response_status' => $response['response_status']]);
-//        }
-//    }
-}
--- app/addons/paytabs/init.php
+++ app/addons/paytabs/init.php
@@ -3,7 +3,3 @@
 if (!defined('BOOTSTRAP')) {
     die('Access denied');
 }
-
-//fn_register_hooks(
-//    'change_order_status'
-//);
